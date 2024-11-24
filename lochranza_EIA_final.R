16c91c7c0235d80328ba6db779ce24e950584a17
c-noonan-02 <charlottenoonan@outlook.com>
2024-11-24 22:48:45 +0000

    Set up of R script for the results section of the EIA. Basic plan outlined.

---
+++ lochranza_EIA_final.R
@@ -0,0 +1,17 @@
+# clear environment
+rm(list=ls())
+
+# import packages needed
+library(ggplot2)
+# ...
+
+# import full data set
+arran_data_full <- read.csv('.\Data\arran_data_full.csv')
+# check the data set
+head(arran_data_full)
+
+# visualise some of the data
+
+# ANALYSIS OF INVERTEBRATE BIODIVERSITY 
+
+# ANALYSIS OF VERTEBRATE BIODIVERSITY

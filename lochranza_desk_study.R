96a79c6e92e06b8cc5ea7491a29ea8f351d3eab9
c-noonan-02 <charlottenoonan@outlook.com>
2024-11-29 00:50:52 +0000

    Reformatted the data again, and exported.

--- lochranza_desk_study.R
+++ lochranza_desk_study.R
@@ -91,5 +91,41 @@ NBN_data_rearranged <- NBN_data %>%
          "Red List - Critically Endangered",
          "slope")
 
+NBN_data_rearranged <- NBN_data_rearranged %>%
+  # Create a new column to indicate the slope (North = 'N', South = 'S')
+  mutate(slope2 = case_when(
+    grepl("North", slope, ignore.case = TRUE) ~ "N",
+    grepl("South", slope, ignore.case = TRUE) ~ "S",
+    TRUE ~ NA_character_
+  )) %>%
+  # Remove records with missing Slope data
+  filter(!is.na(slope)) %>%
+  
+  # Group by Species and Source Columns and summarize slope presence (N, S, B)
+  group_by(Species.Name) %>%
+  summarize(
+    across(
+      c("Biodiversity Action Plan UK list of priority species",
+        "Red List - Endangered", 
+        "RSPB Priority Species", 
+        "Scottish Biodiversity List", 
+        "Invasive Species", 
+        "Red List - Critically Endangered"),
+      ~ case_when(
+        all(slope2 == "N" & !is.na(.)) ~ "N",
+        all(slope2 == "S" & !is.na(.)) ~ "S",
+        any(slope2 == "N") & any(slope2 == "S") ~ "B",
+        TRUE ~ NA_character_
+      ),
+      .names = "slope_{.col}"  # Renaming columns with a Slope prefix
+    ),
+    Site = paste(unique(slope), collapse = ", ")
+  ) %>%
+  ungroup()
+
+# remove the no longer needed 'slope' column
+NBN_data_rearranged <- NBN_data_rearranged %>%
+  select(-Site)
+
 # save as 'NBN_atlas_data_summarised.csv'
 write.csv(NBN_data_rearranged, "./Data/NBN_atlas_data_summarised.csv", row.names = F)
